1b1e2ab34636f0e48f6fd6119a8dacea0e3ec7d6
Владимир Силин <sva@itx24.ru>
2024-09-16 21:19:56 +0700

    add bootstrap

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
 
@@ -9,4 +9,4 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Flask!'
+    return render_template('index.html')
